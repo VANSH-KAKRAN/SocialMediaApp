c5ad4612416d040b8b7f244ee6557fd73b98e536
Vansh Kakran <166276299+VANSH-KAKRAN@users.noreply.github.com>
2025-01-25 12:00:17 +0530

    Update MongoConn.js for privacy

--- Backend/Connection/MongoConn.js
+++ Backend/Connection/MongoConn.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const MongoConnection = mongoose.connect(
-  "mongodb+srv://vanshkakran82:jaatgodff1234@cluster-1.buada.mongodb.net/userData?retryWrites=true&w=majority&appName=Cluster-1"
+  "mongodb+srv://username:password@clusterName.buada.mongodb.net/CollectionName?retryWrites=true&w=majority&appName=Cluster-1"
 ).then(()=>{
     console.log("MongoDB Connected...");
 }).catch(()=>{
